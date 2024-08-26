79826ff17f8cbe809bcdb161639492fd43d9bb1a
Sahib3 <59211829+Sahib3@users.noreply.github.com>
2024-08-26 14:43:52 -0600

    Changed generator functionality to generate a random 16 character length password using the characters string and for loop

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const passEl = document.querySelector("#generated-password")
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+let password = ""
 
-function generator () {
-    passEl.textContent = "Hello" 
+function generator() {
+    for(let i=0;i<15;i++) {
+       password += characters[Math.floor(Math.random() * characters.length-1)] 
+    }
+    console.log(password)
+    return password
+}
+
+
+function assigner() {
+    passEl.textContent += "Hello" 
 }

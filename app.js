fdfe97a886c233a4b89199c0abb2f8a98b9b039e
Sahib3 <59211829+Sahib3@users.noreply.github.com>
2024-08-28 16:04:02 -0600

    Added comments to generator, assinger and reset functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const passEl = document.querySelector("#generated-password")
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 let password = ""
 
+// function that generates the seucre password by randomly selecting 15 characters from the characters variable 
 function generator() {
     for(let i=0;i<15;i++) {
        password += characters[Math.floor(Math.random() * characters.length-1)] 
@@ -12,11 +13,12 @@ function generator() {
 
 generator()
 
-
+// updates the html of the page with the generated password
 function assigner() {
      passEl.textContent += password
 }
 
+//resets the password and html by reloading the page
 function reset() {
     location.reload()
 }

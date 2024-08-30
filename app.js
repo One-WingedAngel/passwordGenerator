c67153a3878a25e180f638d3ded985758b4b1a95
Sahib3 <59211829+Sahib3@users.noreply.github.com>
2024-08-29 18:36:58 -0700

    Changed password generating method from using onclick to eventlistners and removed the reset function so now every time the generate button is clicked it will generate a new password

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const passEl = document.querySelector("#generated-password")
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-let password = ""
 
 // function that generates the seucre password by randomly selecting 15 characters from the characters variable 
 function generator() {
+    let password = ""
+
     for(let i=0;i<15;i++) {
        password += characters[Math.floor(Math.random() * characters.length-1)] 
     }
@@ -11,16 +12,13 @@ function generator() {
     return password
 }
 
-generator()
+
 
 // updates the html of the page with the generated password
 function assigner() {
-     passEl.textContent += password
-}
-
-//resets the password and html by reloading the page
-function reset() {
-    location.reload()
+    const password =generator()
+     passEl.textContent = password
 }
 
 
+document.getElementById("btn-gen").addEventListener("click",assigner)
